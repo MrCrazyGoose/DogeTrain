61d4962ed23164865f00ac66f4932a918062caed
DOGE <87092791+MrCrazyGoose@users.noreply.github.com>
2022-01-29 17:58:45 -0500

    Add files via upload
    gamemodes update: dogetrain 1.1

--- index.md
+++ index.md
@@ -1,26 +1,3 @@
 ## DogeTrain
-## Bite those Dogecoins like you wish you could in real life
 
 [Play](snake.html)
-
-------------------------------------------------------------------------
-
-Update log:
-12/04/2021: Release (1.0)
-12/06/2021: This page (1.0)
-
-Further updates will be recorded here!
-
-Next update: Gamemodes (1.1)
-
-------------------------------------------------------------------------
-
-Cringe creator's note:
-By making this Dogecoin eating simulation game, I hope to inspire people to chase their goals and create their future with cryptocurrencies. 
-
-Real creator's note:
-Dogecoin + Snake = Dogetrain. Basically, I started to play snake and found it fun so I re-created it and added a doge theme to it because I thought it would be kind of funny.
-
-------------------------------------------------------------------------
-
-What I will work on next: I can not currently identify the answer to this question. 
--- snake.js
+++ snake.js
@@ -1,217 +1,469 @@
-// -------------------- game settings --------------------
-const SIZE = 30;       // px
-const DELAY = 75;      // ms
-const FOOD_COUNT = 5;
-
-
-// -------------------- DOM (Document Object Model = HTML) and other JS stuff --------------------
-let canvas = document.querySelector("canvas");
-let ctx = canvas.getContext("2d");
-let scoreElement = document.querySelector("#score");
-let hsElement = document.querySelector("#highscore");
-let intervalId = null;
-
-// load images
-const dedImg = document.createElement("img");
-const moneysImg = document.createElement("img");
-const dogeRight = document.createElement("img");
-
-dedImg.src = "assets/gameover.jpg";
-moneysImg.src = "assets/dogecoin.png";
-dogeRight.src = "assets/dogeright.png";
-
-// load song(s) and sound(s)
-const bestSong = new Audio("assets/bestsong.mp3");
-bestSong.loop = true;
-bestSong.volume = 1;  // 1 is max, 0 is muted.
-// bestSong.addEventListener("canplaythrough", function() {});
-
-
-// -------------------- other constants --------------------
-const H = canvas.height / SIZE;  // height in blocks
-const W = canvas.width / SIZE;   // width in blocks
-
 // enum ("enumeration")
 const RIGHT = 1;
-const DOWN = 2
+const DOWN = 2;
 const LEFT = 3;
 const UP = 4;
 
+// load images
+const dedImg = document.createElement("img");
+dedImg.src = "assets/gameover.jpg";
+const moneysImg = document.createElement("img");
+moneysImg.src = "assets/dogecoin.png";
+const dogeRight = document.createElement("img");
+dogeRight.src = "assets/dogeright.png";
 
-// -------------------- variables representing the game state --------------------
-let dir = null;
-let snake = [];
-let moneys = [];
-let nextDir = null;  // the direction the snake should change to on next "frame", or null if no change in direction
-let score = 420;
-let highScore = 0;
+// load song
+const bestSong = new Audio("assets/bestsong.mp3");
+bestSong.loop = true;
+bestSong.volume = 0.75;  // 1 is max, 0 is muted.
+// bestSong.addEventListener("canplaythrough", function() {});
 
+// // load sound effects
+const airhornSound = new Audio("assets/airhorn.mp3");
+bestSong.loop = false;
+bestSong.volume = 0.75;
 
-// -------------------- graphics/UI functions --------------------
-function drawBackground() {
-	ctx.fillStyle = "lightblue";
-	ctx.fillRect(0, 0, 600, 600);
-}
+const coinSound = new Audio("assets/coin.mp3");
+bestSong.loop = false;
+bestSong.volume = 0.75;
 
+const youCantSeeMe = new Audio("assets/jhoncena.mp3");
+bestSong.loop = false;
+bestSong.volume = 0.75;
 
-// -------------------- game functions --------------------
-function spawnCoin() {
-	let x = Math.floor(Math.random() * W);
-	let y = Math.floor(Math.random() * H);
+const moneySound = new Audio("assets/money.mp3");
+bestSong.loop = false;
+bestSong.volume = 0.75;
 
-	// food on snake bad
-	for (let piece of snake) {
-		if (piece.x === x && piece.y === y) {
-			spawnCoin();
-			return;  // end this function
-		}
-	}
+const oofSound = new Audio("assets/oof.mp3");
+bestSong.loop = false;
+bestSong.volume = 1;
 
-	// food on food bad
-	for (let piece of moneys) {
-		if (piece.x === x && piece.y === y) {
-			spawnCoin();
-			return;  // end this function
-		}
-	}
-	moneys.push({x: x, y: y});
-}
+// ----------------------------------- CLASSES -----------------------------------
 
-function deyDed() {
-	clearInterval(intervalId);  // stop the "setInterval" loop
-	console.log("dey ded");
-	drawBackground();
-	ctx.drawImage(dedImg, 0, 0, 600, 600);
+// Logic for a game of DogeTrain
+class DogeTrain {
 
-	// update high score, maybe
-	if (score > highScore) {
-		highScore = score;
-		localStorage.setItem("highScore", highScore);
-		hsElement.innerText = highScore;
-		console.log("New High Score: " + highScore);
-	}
-}
+	constructor(W, H, initialFoodCount) {
+		// variables representing the game state
+		this.dir = null;
+		this.nextDir = null;  // the direction the snake should change to on next "frame", or null if no change in direction
+		this.snake = [];  // head and body pieces of "snake"
+		this.moneys = [];  // positions of "coins"
+		this.score = 420;
+		this.highScore = 0;
+		this.gameOver = false;
+		this.W = W;  // width of game board/grid measured in cell
+		this.H = H;  // height of game board/grid measured in cell
 
-function updateAndDraw() {
-	// ----- Draw -----
-	drawBackground();
+		// event handlers
+		this.onBoomWithFood = null;
 
-	const N = SIZE/4;  // how many extra pixels on each side?
-
-	// draw food
-	for (let piece of moneys) {
-		ctx.drawImage(moneysImg, piece.x * SIZE - N, piece.y * SIZE - N, SIZE + 2 * N, SIZE + 2 * N);
-	}
-
-	// draw doge
-	// draw head
-	let piece = snake[0];
-	ctx.drawImage(dogeRight, piece.x * SIZE - N, piece.y * SIZE - 2, SIZE + 2 * N, SIZE + 2 *N);
-	
-	// draw body
-	ctx.strokeStyle = "red";
-	ctx.lineWidth = SIZE / 3;
-	ctx.beginPath();
-	ctx.moveTo(SIZE * (snake[0].x + snake[1].x + 1)/2, SIZE * (snake[0].y + snake[1].y + 1)/2);
-	for(let i=1; i+1 < snake.length; i++) {
-		ctx.lineTo(SIZE * (snake[i].x + snake[i+1].x + 1)/2, SIZE * (snake[i].y + snake[i+1].y + 1)/2);
-	}
-	ctx.stroke();
-
-
-	// ----- Music -----
-	if (dir !== null)
-		bestSong.play();
-
-	// ---- Update -----
-	if (nextDir !== null) {
-		dir = nextDir;   // update direction (only once per frame)
-		nextDir = null;  // clear nextDir "buffer"
-	}
-
-	let tail = {
-		x: snake[snake.length - 1].x,
-		y: snake[snake.length - 1].y
-	};
-
-	if (dir !== null) {
-		// move body of snake
-		for (let i = snake.length - 1; i >= 1; i--) {
-			snake[i].x = snake[i - 1].x;
-			snake[i].y = snake[i - 1].y;
-		}
-
-		// move head snake
-		if (dir == RIGHT)
-			snake[0].x = snake[0].x + 1;
-		else if (dir == LEFT)
-			snake[0].x = snake[0].x - 1;
-		else if (dir == UP)
-			snake[0].y = snake[0].y - 1;
-		else if (dir == DOWN)
-			snake[0].y = snake[0].y + 1;
+		// load high score from "localStorage"
+		this.highScore = localStorage.getItem("highScore");
+		if (this.highScore === null)
+			this.highScore = -Infinity;  // if there is no high score yet
+		else
+			this.highScore = Number(this.highScore);  // convert string into Number
 		
-		//boom with self?
-		for(let i = 1; i < snake.length; i++) {
-			if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
-				deyDed();
-				return;  // nothing after deth
+		// spawn "snake"
+		this.snake.push({ x: 5, y: Math.floor(H/2) });  // doge head: snake[0]
+		this.snake.push({ x: 4, y: Math.floor(H/2) });  // doge body: snake[i] where i > 0
+		this.snake.push({ x: 3, y: Math.floor(H/2) });
+
+		// spawn inital food
+		for (let i = 1; i <= initialFoodCount; i++)
+			this.spawnCoin();
+	}
+
+	spawnCoin() {
+		let x = Math.floor(Math.random() * this.W);
+		let y = Math.floor(Math.random() * this.H);
+
+		// food on snake bad
+		for (let piece of this.snake) {
+			if (piece.x === x && piece.y === y) {
+				this.spawnCoin();
+				return;  // end this function
 			}
 		}
-		//you did not look at the road!!! (boring version: you hit the edge of the screen)
-		if (snake[0].y < 0 || snake[0].x < 0 || snake[0].y >= H || snake[0].x >= W) {
-			deyDed();
-			return;
+
+		// food on food bad
+		for (let piece of this.moneys) {
+			if (piece.x === x && piece.y === y) {
+				this.spawnCoin();
+				return;  // end this function
+			}
+		}
+		this.moneys.push({x: x, y: y});
+	}
+
+	deyDed() {
+		this.gameOver = true;
+
+		// update high score, maybe
+		if (this.score > this.highScore) {
+			this.highScore = this.score;
+			localStorage.setItem("highScore", this.highScore);
+			// console.log("New High Score: " + this.highScore);
 		}
 	}
-	
-	// boom with food?
-	for (let i = 0; i < moneys.length; i++) {
-		if (moneys[i].x === snake[0].x && moneys[i].y === snake[0].y) {
-			moneys.splice(i, 1);
-			snake.push(tail);
-			spawnCoin()
-			score += 69;
-			scoreElement.innerText = score;
-			break;
+
+	// called once per frame (before "draw") to update game state
+	update() {
+		// console.log(this.snake[0], this.dir, " <-- ", this.nextDir);
+		let snake = this.snake;
+
+		if (this.nextDir !== null) {
+			this.dir = this.nextDir;   // update direction (only once per frame)
+			this.nextDir = null;  // clear nextDir "buffer"
+		}
+
+		let tail = {
+			x: snake[snake.length - 1].x,
+			y: snake[snake.length - 1].y
+		};
+
+		if (this.dir !== null) {
+			// move body of snake
+			for (let i = snake.length - 1; i >= 1; i--) {
+				snake[i].x = snake[i - 1].x;
+				snake[i].y = snake[i - 1].y;
+			}
+
+			// move head snake
+			if (this.dir == RIGHT)
+				snake[0].x = snake[0].x + 1;
+			else if (this.dir == LEFT)
+				snake[0].x = snake[0].x - 1;
+			else if (this.dir == UP)
+				snake[0].y = snake[0].y - 1;
+			else if (this.dir == DOWN)
+				snake[0].y = snake[0].y + 1;
+			
+			//boom with self?
+			for(let i = 1; i < snake.length; i++) {
+				if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+					this.deyDed();
+					return;  // nothing after deth
+				}
+			}
+			//you did not look at the road!!! (boring version: you hit the edge of the screen)
+			if (snake[0].y < 0 || snake[0].x < 0 || snake[0].y >= this.H || snake[0].x >= this.W) {
+				this.deyDed();
+				return;
+			}
+		}
+		
+		// boom with food?
+		for (let i = 0; i < this.moneys.length; i++) {
+			if (this.moneys[i].x === snake[0].x && this.moneys[i].y === snake[0].y) {
+				this.moneys.splice(i, 1);
+				snake.push(tail);
+				this.spawnCoin()
+				this.score += 69;
+				if (this.onBoomWithFood !== null)
+					this.onBoomWithFood({x: snake[0].x, y: snake[0].y});
+				break;
+			}
+		}
+	}
+
+}
+
+// Provides a GUI for a DogeTrain object
+class DogeTrainGUI {
+
+	constructor(dogeTrain, parentElement) {
+		this.train = dogeTrain;
+		
+		// DOM (Document Object Model = HTML) and other JS stuff
+		let canvas = parentElement.querySelector("#DogeTrain canvas");
+		this.SIZE = {
+			x: canvas.width / this.train.W,  // size of each "block" in px (horizontal)
+			y: canvas.height / this.train.H  // size of each "block" in px (vertical)
+		};
+		this.SIZE.min = Math.min(this.SIZE.x, this.SIZE.y);
+		this.SIZE.max = Math.max(this.SIZE.x, this.SIZE.y);
+		this.SIZE.avg = (this.SIZE.x + this.SIZE.y) / 2;
+		
+		this.ctx = canvas.getContext("2d");
+
+		this.scoreElement = parentElement.querySelector(".score");
+		
+		this.hsElement = parentElement.querySelector(".highscore");
+		this.hsElement.innerText = this.train.highScore;
+
+		this.intervalId = null;
+
+		this.bestSongPlaying = false;
+		this.nextMoney = 1000;  // score goal to play next MoneySound
+		
+		// controls
+		let self = this;
+		let train = this.train;
+		document.addEventListener("keydown", function(event) {
+			console.log(event.key);
+			if (train.gameOver)
+				location.reload();  // TODO
+			
+			if (event.key === "w" || event.key === "W" || event.key === "ArrowUp") {
+				if (train.dir !== DOWN) {
+					train.nextDir = UP;
+				}
+				event.preventDefault();
+			}
+			else if ((event.key === "a" || event.key === "A" || event.key === "ArrowLeft")) {
+				if (train.dir !== RIGHT) {
+					train.nextDir = LEFT;
+				}
+				event.preventDefault();
+			}
+			else if ((event.key === "s" || event.key === "S" || event.key === "ArrowDown")) {
+				if (train.dir !== UP) {
+					train.nextDir = DOWN;
+				}
+				event.preventDefault();
+			}
+			else if ((event.key === "d" || event.key === "D" || event.key === "ArrowRight")) {
+				if (train.dir !== LEFT) {
+					train.nextDir = RIGHT;
+				}
+				event.preventDefault();
+			}
+			else if (event.key === " ") {
+				self.playSound(airhornSound)
+				event.preventDefault();
+			}
+			else {
+				train.deyDed();
+				self.stop();
+			}
+		});
+
+		// event handlers
+		this.train.onBoomWithFood = function(event) {
+			self.boomWithFood(event);
+		};
+	}
+
+	drawBackground() {
+		this.ctx.fillStyle = "lightblue";
+		this.ctx.fillRect(0, 0, 600, 600);
+	}
+
+	playSound(sound, startTime) {
+		// if (sound !== oofSound)
+		// 	return;
+		
+		if (startTime === undefined)
+			startTime = 0;
+		
+		sound.pause();
+		sound.currentTime = startTime;
+		sound.play();
+	}
+
+	start(delay) {
+		this.draw();
+
+		let self = this;
+		this.intervalId = setInterval(function() {
+			self.train.update();
+			self.draw();
+		}, delay);
+	}
+
+	// called once per frame (after "update") to redraw GUI elements that may have changed
+	draw() {
+		this.hsElement.innerText = this.train.highScore;  // update hish score each frame. (TODO: make more efficient with "callback"/"event listener")
+
+		if (this.train.gameOver) {
+			this.stop();
+			return;
+		}
+
+		let snake = this.train.snake;
+		
+		this.drawBackground();
+
+		// how many extra pixels on each side?
+		const MARGIN = {
+			x: this.SIZE.x / 4,
+			y: this.SIZE.y / 4
+		};
+
+		// draw food
+		for (let piece of this.train.moneys) {
+			this.ctx.drawImage( moneysImg,
+					piece.x * this.SIZE.x - MARGIN.x,
+					piece.y * this.SIZE.y - MARGIN.y,
+					this.SIZE.x + 2 * MARGIN.x,
+					this.SIZE.y + 2 * MARGIN.y );
+		}
+
+		// draw doge
+		// draw head
+		let piece = snake[0];
+		this.ctx.drawImage( dogeRight,
+				piece.x * this.SIZE.x - MARGIN.x,
+				piece.y * this.SIZE.y - MARGIN.y,
+				this.SIZE.x + 2 * MARGIN.x,
+				this.SIZE.y + 2 * MARGIN.y );
+		
+		// draw body
+		this.ctx.strokeStyle = "red";
+		this.ctx.lineWidth = this.SIZE.avg / 3;
+		this.ctx.beginPath();
+		this.ctx.moveTo(this.SIZE.x * (snake[0].x + snake[1].x + 1)/2, this.SIZE.y * (snake[0].y + snake[1].y + 1)/2);
+		for(let i=1; i+1 < snake.length; i++) {
+			this.ctx.lineTo(this.SIZE.x * (snake[i].x + snake[i+1].x + 1)/2, this.SIZE.y * (snake[i].y + snake[i+1].y + 1)/2);
+		}
+		this.ctx.stroke();
+
+		// update score every frame
+		this.scoreElement.innerText = this.train.score;
+
+		// ----- Music -----
+		if (!this.bestSongPlaying && this.train.dir !== null) {
+			this.playSound(bestSong, 0);
+			this.playSound(youCantSeeMe)
+			this.bestSongPlaying = true;
+		}
+		
+		if (this.train.score >= this.nextMoney) {
+			this.nextMoney += 1000;
+			this.playSound(moneySound, 0);
+		}
+	}
+
+	boomWithFood(event) {
+		console.log(this);
+		this.playSound(coinSound, 0.13);
+	}
+
+	stop() {
+		clearInterval(this.intervalId);  // stop the "setInterval" loop
+		if (this.train.gameOver) {
+			this.drawBackground();
+			this.ctx.drawImage(dedImg, 0, 0, 600, 600);
+			
+			this.playSound(oofSound, 0.5)
 		}
 	}
 }
 
-// -------------------- controlls --------------------
-document.addEventListener("keydown", function(event) {
-	// console.log(event.key);
-	if ((event.key === "w" || event.key === "W" || event.key === "ArrowUp") && dir !== DOWN)
-		nextDir = UP;
-	else if ((event.key === "a" || event.key === "A" || event.key === "ArrowLeft") && dir !== RIGHT)
-		nextDir = LEFT;
-	else if ((event.key === "s" || event.key === "S" || event.key === "ArrowDown") && dir !== UP)
-		nextDir = DOWN;
-	else if ((event.key === "d" || event.key === "D" || event.key === "ArrowRight") && dir !== LEFT)
-		nextDir = RIGHT;
+
+// -------------------- Extra stuff --------------------
+function lerp(x_val, x0, y0, x1, y1) {
+	let rate = (y1 - y0) / (x1 - x0);  // slope formula
+	return rate * (x_val - x0) + y0;   // point-slope form
+}
+
+// settings
+class Setting {
+	constructor(name, initValue, onChange, valueTransform) {
+		if (!valueTransform)
+			valueTransform = (x) => x;  // identity function
+
+		this.name = name;
+		this.itemName = "setting." + this.name;  // name of this setting in "localStorage"
+		this.initValue = initValue;
+		this.valueTransform = valueTransform;
+		this.slider = document.querySelector("#" + name);  // input type=range Element
+		this.vis = document.querySelector(`label[for=${name}] .value`);  // "visible value" Element
+		this.value = valueTransform(initValue);
+
+		let self = this;
+		function updateValue(event) {
+			let value = event.currentTarget.value;
+			self.vis.innerText = value;
+			self.value = valueTransform(Number(value));
+			localStorage.setItem(self.itemName, value);  // save new value to "localStorage"
+			console.log(name, self.vis.innerText, self.value, localStorage.getItem(self.itemName));  // DEBUG
+			if (onChange)
+				onChange();
+		}
+		for (let eventType of ["mousemove", "change"]) {
+			this.slider.addEventListener(eventType, updateValue);
+		}
+		
+		// load previous setting value from "localStorage" or use default
+		let storedValue = localStorage.getItem(this.itemName);
+		if (storedValue !== null) {
+			this.change(storedValue);
+		} else {
+			this.slider.value = initValue;
+			this.vis.innerText = initValue;
+		}
+	}
+
+	change(value) {
+		this.slider.value = value;
+		this.slider.dispatchEvent(new Event("change"));
+	}
+
+	reset() {
+		this.change(this.initValue);
+	}
+}
+
+let sizeSetting = null;
+let speedSetting = null;
+let foodSetting = null;
+
+sizeSetting = new Setting("size", 20, function() {
+	if (foodSetting !== null)
+		foodSetting.updateMax();
+	if (speedSetting !== null)
+		speedSetting.value = speedSetting.valueTransform(Number(speedSetting.slider.value));
 });
 
+speedSetting = new Setting("speed", 20, null, function(x) {
+	// x (parameter): value in slider, e.g. 1 to 100
+	// y: time to travel across to world (in Hz = 1/seconds), e.g. (1/20, 4)
+	// return: delay (in ms) for "setInterval()"
+	let y = lerp(x, 1, 1/20.0, 100, 1/0.25);
+	return 1000 / (sizeSetting.value * y);
+});
 
-// -------------------- Initialize game --------------------
+foodSetting = new Setting("food", 10);
+foodSetting.updateMax = function() {
+	foodSetting.slider.max = sizeSetting.value * sizeSetting.value - 4;
+	foodSetting.slider.value = Math.min(foodSetting.slider.value, foodSetting.slider.max);
+	foodSetting.vis.innerText = foodSetting.slider.value;
+	foodSetting.value = foodSetting.valueTransform(Number(foodSetting.slider.value));
+};
+foodSetting.updateMax();
 
-// high score
-highScore = localStorage.getItem("highScore");
-if (highScore === null)
-	highScore = -Infinity;  // if there is no high score yet
-else
-	highScore = Number(highScore);  // convert string into Number
-hsElement.innerText = highScore;
-console.log("High Score: " + highScore);
 
-// spawn "snake"
-snake.push({ x: 5, y: Math.floor(H/2) });  // doge head: snake[0]
-snake.push({ x: 4, y: Math.floor(H/2) });  // doge body: snake[i] where i > 0
-snake.push({ x: 3, y: Math.floor(H/2) });
+// start game button
+document.querySelector("#start").addEventListener("click", function() {
+	let logic = new DogeTrain(sizeSetting.value, sizeSetting.value, foodSetting.value);
+	let gui = new DogeTrainGUI(logic, document.querySelector("#DogeTrain"));
+	gui.start(speedSetting.value);
+	let button = document.querySelector("#start")
+	button.disabled = true
+});
 
-// spawn inital food
-for (let i=1;i<=FOOD_COUNT;i++)
-	spawnCoin();
+// reset settings button
+document.querySelector("#resetSettings").addEventListener("click", function() {
+	for (let setting of [sizeSetting, speedSetting, foodSetting])
+		setting.reset();
+});
 
-// -------------------- start game --------------------
-updateAndDraw();
-intervalId = setInterval(updateAndDraw, DELAY);
+// Inital drawing on Canvas before "new DogeTrainGUI()" is instantiated (i.e. before the user clicks "start")
+const dogeTrain = document.createElement("img");
+dogeTrain.addEventListener("load", function() {
+	let canvas = document.querySelector("#DogeTrain canvas")
+	canvas.getContext("2d").drawImage(dogeTrain, 0, 0, canvas.width, canvas.height);
+});
+
+dogeTrain.src = "assets/DogeTrain.png";
+
+
+
+
+
+
+// LINE 420
